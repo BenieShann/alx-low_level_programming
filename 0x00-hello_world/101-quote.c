78c49171c2de0883b294473db6e92702c3337607
BenieShann <shann.vana@gmail.com>
2023-02-16 19:37:03 +0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *  * main - program entry point
- *   * Return: 1
- *    */
+ * main - program entry point
+ * Return: 1
+ */
 int main(void)
 {
 		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
-}#include <stdio.h>
-#include <unistd.h>
-/**
- *  * main - program entry point
- *   * Return: 1
- *    */
-int main(void)
-{
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+		return (1);
 }
