bc529694946d3c98c8901e8d75ad8f8b7833684a
BenieShann <shann.vana@gmail.com>
2023-02-16 19:02:35 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 /**
  *main - Entry point
- *Retun: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
-	puts("\"Programming is like building a multilingual puzzle");	
-        return (0);
-
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
-
