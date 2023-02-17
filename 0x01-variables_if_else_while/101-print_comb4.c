76a3f9a7b83f0f90afe161a9b294326c750a6efb
BenieShann <shann.vana@gmail.com>
2023-02-17 19:27:30 +0300

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,19 +15,20 @@ int main(void)
 		for (hundred = '0'; hundred <= '9'; hundred++) /*hundreds place*/
 		{
 	        	for (ten = (hundred + 1); ten <= '9'; ten++) /*tens=100s+1*/
-	        	{																     for (one = (ten + 1); one <= '9'; one++) /*ones*/
+	        	{				    
+			       	for (one = (ten + 1); one <= '9'; one++) /*ones*/
 				{
-		 		              																putchar(hundred);
-			 												                  				putchar(ten);																													putchar(one);
-																	         																			if (hundred != '7' || ten != '8' || one != '9')
-																																											{
-																																																	putchar(',');
-																																																						putchar(' ');
-																																																										}
-																																									}
-																			}
-											}
-						putchar('\n');
-
-							return (0);
+		 		          putchar(hundred);							                  				   
+					  putchar(ten);											                  
+				  	  putchar(one);
+					 
+					 if (hundred != '7' || ten != '8' || one != '9')
+				 	{
+						putchar(',');
+						putchar(' ');	
+						}
+					}
+				}
+						putchar('\n')
+						return (0);
 }
