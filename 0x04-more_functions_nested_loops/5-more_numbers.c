a34ae4a94e8eb6150a78a6ffed5a2b79e0cf32ac
BenieShann <shann.vana@gmail.com>
2023-02-23 14:32:04 +0300

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,13 +12,12 @@ void more_numbers(void)
 
 	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; l <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
 			_putchar('1');
 			_putchar(j % 10 + '0');
 		}
-		_putchar('\n');
+			_putchar('\n');
 	}
 }
-
