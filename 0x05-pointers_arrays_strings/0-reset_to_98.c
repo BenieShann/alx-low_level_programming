0d0124a896be0738f5cfdafe57e6223777a28335
BenieShann <shann.vana@gmail.com>
2023-02-27 15:37:20 +0300

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes the pointer and updates the value
+ * @n: int to check
+ * #BenieShann
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+s
+	*n = 98;
+
+}
