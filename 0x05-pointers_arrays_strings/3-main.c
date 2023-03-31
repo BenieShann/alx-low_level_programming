812e68c7ae89427096850a81d16be3620f9917fc
BenieShann <shann.vana@gmail.com>
2023-03-31 20:24:27 +0300

    A function that prints a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str1;
+	char *str2;
+	char *str3;
+
+	str1 = "Kevin is a very very bad personnnnnnnnn,\n";
+	str2 = "amesema mimi na Eve tunasumbua.\n";
+	str3 = "are waiting for a sincere apology - Shann.\n";
+	_puts(str1);
+	_puts(str2);
+	_puts(str3);
+	return (0);
+}
