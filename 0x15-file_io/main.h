985b7a4e17767553237a5e504b3a2782d11d048b
BenieShann <shann.vana@gmail.com>
2023-05-08 17:14:41 +0300

    Readme

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,15 @@
 #ifndef main_h
 #define main_h
+
 #include <stdio.h>
 #include <unistd.h>
+
+#include "string.h"
+
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include "string.h"
 
 /**
  * struct copy_struct - struct for cp
@@ -17,19 +20,19 @@
  * @wt:write stode
  * @from_file:open from file
  * @to_file: open of to file
- * @buffer: buffer to read to
+ * @buffer: buffer to read to.
  *
-*/
+ */
 
 typedef struct copy_struct
 {
 	char *from;
+	char *buffer;
 	char *to;
 	ssize_t rd;
 	ssize_t wt;
 	int from_file;
 	int to_file;
-	char *buffer;
 } copy_struct;
 
 int _putchar(char c);
@@ -37,4 +40,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /*Benie*/
+#endif /*Shann*/
