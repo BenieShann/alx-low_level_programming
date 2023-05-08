2f19aa0e4c9fab9e8541c756454da11843fdd669
BenieShann <shann.vana@gmail.com>
2023-05-08 17:47:12 +0300

    A function that reads a text file and prints it to the POSIX standard output.

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,42 @@
+#ifndef main_h
+#define main_h
+
+#include "string.h"
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * struct copy_struct - struct for cp
+ *
+ * @from: from file
+ * @to: to file
+ * @rd:read store
+ * @wt:write stode
+ * @from_file:open from file
+ * @to_file: open of to file
+ * @buffer: buffer to read to.
+ *
+ */
+
+typedef struct copy_struct
+{
+	char *from;
+	char *to;
+	char *buffer;
+	ssize_t rd;
+	ssize_t wt;
+	int from_file;
+	int to_file;
+} copy_struct;
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /*Shann*/
