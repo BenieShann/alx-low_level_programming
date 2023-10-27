4316829212a2f0b2509e711bfac20353f314b161
BenieShann <shann.vana@gmail.com>
2023-10-27 19:05:11 +0300

    all files

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - Prints the name of the program
  * followed by a new line
@@ -7,6 +8,7 @@
  * @argv : string of argumenst passed to main fcn
  * Return: Always 0 (Success)
  */
+
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - Prints the number of arguments passed to the program
  * followed by a new line
@@ -7,6 +8,7 @@
  * @argv : string of argumenst passed to main fcn
  * Return: Always 0 (Success)
  */
+
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
 	printf("%d\n", argc - 1);
